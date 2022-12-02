8018f063ba6674d9c17048987b4d41cd92d3c72f
JimenezMario <81141032+JimenezMario@users.noreply.github.com>
2022-12-01 21:31:40 -0600

    TablasDeMultiplicar

---
+++ TablasDeMultiplicar.java
@@ -0,0 +1,58 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class TablasDeMultiplicar {
+
+	public static void main(String[] args) {
+		/*
+		Scanner sc = new Scanner(System.in);
+		
+		int num = 0;
+		
+		System.out.println("ingresa un numero");
+		num = sc.nextInt();
+		
+		System.out.println("tabla del:" + num);
+		
+		for(int i = 1 ; i <= 10 ; i++) {
+			System.out.println(num + "*" + i + "=" + num * i);
+		}
+		 
+		*/
+		
+		
+		int num1 = 1;
+		int num2 = 2;
+		int num3 = 3;
+		int num4 = 4;
+		
+		
+		
+		
+		System.out.println("tablas del:" + num1);
+	
+		for(int i = 1 ; i <= 10 ; i++) {
+			System.out.println(num1 + "*" + i + "=" + num1 * i);
+		}
+		
+		System.out.println("tablas del:" + num2);
+		
+		for(int i = 1 ; i <= 10 ; i++) {
+			System.out.println(num2 + "*" + i + "=" + num2 * i);
+		}
+		System.out.println("tablas del:" + num3);
+		
+		for(int i = 1 ; i <= 10 ; i++) {
+			System.out.println(num3 + "*" + i + "=" + num3 * i);
+		}
+		
+		System.out.println("tablas del:" + num4);
+		
+		for(int i = 1 ; i <= 10 ; i++) {
+			System.out.println(num4 + "*" + i + "=" + num4 * i);
+		}
+
+	}
+
+}
